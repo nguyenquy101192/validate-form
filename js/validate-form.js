e8ef405f8ba117793f05da18b3ed7ed7744a566c
QuyNH <nguyenquy101192@gmail.com>
2015-04-02 15:22:13 +0700

    VF-03

--- js/validate-form.js
+++ js/validate-form.js
@@ -1,14 +1,57 @@
 function validateForm(formId) {
-    var formToValidation = document.getElementById(formId);
-    var required = formToValidation.getElementsByClassName('required');
-    console.log(required);
+    var formToValidation = document.getElementById(formId),
+        required = formToValidation.getElementsByClassName('required'),
+        submit = formToValidation.getElementsByTagName('button').submit,
+        reset = formToValidation.getElementsByTagName('button').reset,
+        inputTag = formToValidation.getElementsByTagName('input'),
+        textareaTag = formToValidation.getElementsByTagName('textarea');
 
-    for(var i = 0; i < required.length; i++) {
-//        if(required[i].val() == '') {
-//            alert('null');
-//        }
-        console.log(required[i]);
-    }
+    // Submit click
+    submit.onclick = function(e) {
+        for(var i = 0; i < required.length; i++) {
+            var caseNull = required[i].value;
 
-    return false;
+            // Check null
+            if(caseNull == '') {
+                if(document.getElementById("required") == null){
+                    var messageNode = document.createElement("span");
+                    messageNode.setAttribute('id','required');
+                } else {
+                    var messageNode = document.getElementById("required");
+                }
+
+                messageNode.setAttribute('class', 'error');
+                messageNode.innerHTML = 'This field is required.';
+                required[i].parentNode.insertBefore(messageNode, required[i].nextSibling);
+//                window.setTimeout(function() {
+                    document.getElementById("required").setAttribute('style', 'display: inline');
+//                }, 50);
+            }
+        }
+        e.preventDefault();
+        e.stopImmediatePropagation();
+    };
+
+    // Reset click
+    reset.onclick = function() {
+        // Reset value input
+        for(var i = 0; i < inputTag.length; i++) {
+            inputTag[i].setAttribute('value', '');
+        }
+
+        // Reset value textarea
+        for(var i = 0; i < textareaTag.length; i++) {
+            textareaTag[i].setAttribute('value', '');
+        }
+
+        // Clear error
+        var error = document.getElementsByClassName('error');
+        if(error != null) {
+            for(var i = 0; i < error.length; i++) {
+                error[i].parentNode.removeChild(error[i]);
+            }
+        }
+    };
+
+    return true;
 }
